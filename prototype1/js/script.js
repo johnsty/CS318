e0b12263873e272a241560e338d750ae88981491
Johnson <JOHNSTY@uwec.edu>
2018-04-18 11:50:51 -0500

    More work done on prototype1

--- prototype1/js/script.js
+++ prototype1/js/script.js
@@ -7,7 +7,6 @@ function openNav() {
     document.getElementById("mobile-nav-menu").classList.toggle('open-nav');
     document.getElementById("signature").classList.toggle('open-nav');
     document.getElementById("signature").style.marginLeft = "300px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -19,7 +18,6 @@ function closeNav() {
     document.getElementById("mobile-nav-menu").classList.remove('open-nav');
     document.getElementById("signature").style.marginLeft = "0";
     document.getElementById("signature").classList.remove('open-nav');
-    document.body.style.backgroundColor = "white";
 }
 
 function scrollToID() {
