e3dcc2a7b1134cbc417956e6972fabfa84115105
Ty Johnson <johnsty@uwec.edu>
2018-04-18 10:05:00 -0500

    Prototype1 Start

---
+++ prototype1/js/script.js
@@ -0,0 +1,29 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("click-to-scroll").style.marginLeft = "57%";
+    document.getElementById("mobile-nav-menu").style.marginLeft = "250px";
+    document.getElementById("mobile-nav-menu").classList.toggle('open-nav');
+    document.getElementById("signature").classList.toggle('open-nav');
+    document.getElementById("signature").style.marginLeft = "300px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("click-to-scroll").style.marginLeft = "auto";
+    document.getElementById("mobile-nav-menu").style.marginLeft = "0";
+    document.getElementById("mobile-nav-menu").classList.remove('open-nav');
+    document.getElementById("signature").style.marginLeft = "0";
+    document.getElementById("signature").classList.remove('open-nav');
+    document.body.style.backgroundColor = "white";
+}
+
+function scrollToID() {
+    document.getElementById("mainText").scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+}
